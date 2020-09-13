171a076796e064a8f9aa3980d4ae83bc22ee8e87
Sunhyunseok <sunhs92@gmail.com>
2020-09-13 23:42:25 +0900

    입력 form 만들기 프로젝트

---
+++ vue-manual/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form action="">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">password: </label>
+        <input id="password" type="password">
+      </div>
+      <button>login</button>
+    </form> 
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
