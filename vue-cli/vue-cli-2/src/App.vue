d53f7e3ccba6cde9f7a80711a4dacf17881c364e
Sunhyunseok <sunhs92@gmail.com>
2020-09-13 21:06:05 +0900

    props&event 구현

--- vue-cli/vue-cli-2/src/App.vue
+++ vue-cli/vue-cli-2/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <!-- template root는 하나의 최상위 element만 있어야함 -->
   <div>
-    {{str}}
+    <!-- v-vind:하위컴포넌트에서 프롭스 속성이름="상위컴포넌트 데이터 이름" -->
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"
+    ></app-header>
   </div>
 </template>
 
@@ -14,10 +18,21 @@
 // })
 // cli를 통해 프로젝트를 만들면 export 안의 속성은 function 함수로 연결해서 
 // return으로 새로운 객체를 반환해주는 형식으로 작성헤야함
+
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header':AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str= 'hi';
     }
   }
 }
---
+++ vue-cli/vue-cli-2/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <h1>{{propsdata}}</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function() {
+            //renew 라는 이벤트 보내기
+            this.$emit('renew');
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
