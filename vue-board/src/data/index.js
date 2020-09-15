118d3ab45ff2623315a605526eec3a2a2f5d797b
Sunhyunseok <sunhs92@gmail.com>
2020-09-15 09:58:45 +0900

    vue-board example

---
+++ vue-board/src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    writer: "선현석",
+    title: "vue.js study",
+    content: "vue-board만들기"
+  },
+  {
+    writer: "hyunseok",
+    title: "vue.js study2",
+    content: "vue-board CRUD"
+  },
+  {
+    writer: "sun",
+    title: "vue.js sample",
+    content: "vue-board example"
+  }
+];
